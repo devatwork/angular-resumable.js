8cdd31aff52802e64523f1ab95883b1d70594a3a
Bert Willems <b.willems@premotion.nl>
2013-05-07 22:25:30 +0200

    + initial try to wrap events in $scope.$apply

--- src/angular-resumable.js-service.js
+++ src/angular-resumable.js-service.js
@@ -27,7 +27,7 @@
 			};
 
 			// return the actual resumableJsFactory that is injected in controllers
-			this['$get'] = [function() {
+			this['$get'] = ['$rootScope', function($rootScope) {
 				/**
 				 * Constructor of the AngularResumable class.
 				 * @param {Object} opts A hash object of the configuration passed to the Resumable.js instance.
@@ -41,6 +41,15 @@
 				}
 				AngularResumable.prototype = new Resumable();
 				AngularResumable.prototype.constructor = AngularResumable;
+				AngularResumable.prototype.on = function(event, callback) {
+					self = this;
+					Resumable.prototype.on.call(this, event, function() {
+						var args = arguments;
+						$rootScope.$apply(function() {
+							callback.apply(self, args);
+						});
+					});
+				};
 
 				// returnt the public api of the resumableJsFactory.
 				return {
